bf0c5e7302c671dab5c6fdfa1c5461c2305fabf5
Marissa Zhong <mwz2101@columbia.edu>
2018-06-27 01:22:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -123,13 +123,13 @@ function deleteNodeAt(index, linkedList, collection) {
   return node_before.next;
 }
 
-let firstNode = {name: 'susie', next: 'rkjasj'}
-let secondNode = {name: 'sam', next: 'asnan'}
-let newNode = {name: 'jill', next: ''}
-let lastNode = {name: 'charlie', next: null}
-let collection = {rkjasj: secondNode,
-  asnan: lastNode,
-  whana: firstNode,
-  ajhsak: newNode}
-  let linkedList = 'whana'
-console.log(deleteNodeAt(1, linkedList, collection))
+// let firstNode = {name: 'susie', next: 'rkjasj'}
+// let secondNode = {name: 'sam', next: 'asnan'}
+// let newNode = {name: 'jill', next: ''}
+// let lastNode = {name: 'charlie', next: null}
+// let collection = {rkjasj: secondNode,
+//   asnan: lastNode,
+//   whana: firstNode,
+//   ajhsak: newNode}
+//   let linkedList = 'whana'
+// console.log(deleteNodeAt(1, linkedList, collection))
